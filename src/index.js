85c126c6b4e60fd8bdb67db423d337465ef3836b
onsnov <114084570+onsnov@users.noreply.github.com>
2023-05-27 23:36:23 +0300

    зміна розмітки

--- src/index.js
+++ src/index.js
@@ -18,40 +18,33 @@ let searchValue = '';
 refs.searchForm.addEventListener('submit', onSubmit);
 refs.loadMoreBtn.addEventListener('click', onLoadMore);
 
-
-
 async function onSubmit(e) {
   e.preventDefault();
   const form = e.currentTarget;
   const form_value = form.elements.searchQuery.value.trim();
- 
+
   if (form_value === '  ') {
     refs.loadMoreBtn.classList.add('is-hidden');
     Notify.failure('Щось введи для пошуку');
   } else getPicturesService.query = form_value;
   clearPicturesList();
-   try {
-     const { hits, totalHits } = await getPicturesService.getPictures(form_value);
-    //  console.log(hits);
-     updatePicturesList();
-    
-      if (hits.length === 0) {
-          refs.loadMoreBtn.classList.add('is-hidden');
-          Notify.info(
-        'Sorry, there are no images matching your search query. Please try again.'
-      );
-      
+  getPicturesService.getPictures(form_value)
+    .then(data => {
+      if (data.hits.length === 0) {
+        refs.loadMoreBtn.classList.add('is-hidden');
+        Notify.info(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
       } else {
-        Notify.success(`Hooray! We found ${totalHits} images.`);
-        refs.loadMoreBtn.classList.remove('is-hidden');
-                      
-    } 
-     return hits.reduce((markup, hit) => createMarkup(hit) + markup, '');
-     
-  } catch (err) {
-    
-    onEror(err);
-  }
+        Notify.success(`Hooray! We found ${data.totalHits} images.`);
+      }
+
+      refs.loadMoreBtn.classList.remove('is-hidden');
+      return data.hits.reduce((markup, hit) => createMarkup(hit) + markup, '');
+    })
+    .then(updatePicturesList)
+    .catch(onEror)
+  
   //     .then(data => {
   //   if (data.hits.length === 0) {
   //     refs.loadMoreBtn.classList.add('is-hidden');
@@ -68,9 +61,8 @@ async function onSubmit(e) {
 
   // .then(updatePicturesList)
   // .catch(onEror);
-  
 }
- 
+
 function createMarkup({
   webformatURL,
   largeImageURL,
@@ -79,9 +71,7 @@ function createMarkup({
   views,
   comments,
   downloads,
-  }) 
-{
-  
+}) {
   return `<div class="photo-card">
    <a class='gallery__link' href='${largeImageURL}'><img src="${webformatURL}" alt="${tags}" width=280px 
     height=200px;
@@ -103,11 +93,9 @@ function createMarkup({
  </div>`;
 }
 
-
-
 function updatePicturesList(markup) {
-  // refs.imageWrapper.insertAdjacentHTML('beforeend', markup);
-  console.log(markup)
+   refs.imageWrapper.insertAdjacentHTML('beforeend', markup);
+  console.log(markup);
 }
 
 function onLoadMore() {
@@ -120,11 +108,10 @@ function onLoadMore() {
           'Sorry, there are no images matching your search query. Please try again.'
         );
       return data.hits.reduce((markup, hit) => createMarkup(hit) + markup, '');
-      
     })
-  
+
     .then(updatePicturesList)
-    
+
     .catch(onEror);
 }
 
