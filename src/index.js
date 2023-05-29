f93294d6682151021e133ef758481b33689c4cd8
onsnov <114084570+onsnov@users.noreply.github.com>
2023-05-29 14:31:25 +0300

    додано await

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import GetPicturesService from './api_fetch.js';
 import './sass/index.scss';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import SimpleLightbox from 'simplelightbox';
 
 import 'simplelightbox/dist/simple-lightbox.min.css';
+import SimpleLightbox from "simplelightbox";
 
 
 
@@ -21,56 +21,46 @@ const lightbox = new SimpleLightbox('.gallery a', {
   captionDelay: 250,
 });
 
+
 refs.searchForm.addEventListener('submit', onSubmit);
 refs.loadMoreBtn.addEventListener('click', onLoadMore);
 
-function onSubmit(e) {
-   e.preventDefault();
+async function onSubmit(e) {
+  e.preventDefault();
   const form = e.currentTarget;
   const form_value = form.elements.searchQuery.value.trim();
 
-  if (form_value ==="" ) {
+  if (form_value === "") {
     refs.loadMoreBtn.classList.add('is-hidden');
     Notify.failure('Щось введи для пошуку');
     return;
   } else { getPicturesService.query = form_value };
+
   clearPicturesList();
+
   refs.loadMoreBtn.classList.add('is-hidden');
-  getPicturesService.getPictures(form_value)
+  const pictures = await getPicturesService.getPictures(form_value)
+  // console.log(pictures);
+  try {
+    if (pictures.hits.length === 0) {
+      refs.loadMoreBtn.classList.add('is-hidden');
+      Notify.info(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+    } else {
+      Notify.success(`Hooray! We found ${pictures.totalHits} images.`);
+      refs.loadMoreBtn.classList.remove('is-hidden');
+    }
+      const result = pictures.hits.reduce(
+        (markup, hit) => createMarkup(hit) + markup,
+        '');
+         updatePicturesList(result);
+  }
   
-    .then(data => {
-      console.log(data)
-      
-      if (data.hits.length ===0) {
-        refs.loadMoreBtn.classList.add('is-hidden');
-        Notify.info(
-          'Sorry, there are no images matching your search query. Please try again.'
-        );
-      } else {
-        Notify.success(`Hooray! We found ${data.totalHits} images.`);
-        refs.loadMoreBtn.classList.remove('is-hidden');
-      }
-       return data.hits.reduce((markup, hit) => createMarkup(hit) + markup, '');
-    })
-    .then(updatePicturesList)
-    .catch(onEror)
+   catch(err){ 
+    onEror(err);
+    }
   
-  //     .then(data => {
-  //   if (data.hits.length === 0) {
-  //     refs.loadMoreBtn.classList.add('is-hidden');
-  //     Notify.info(
-  //       'Sorry, there are no images matching your search query. Please try again.'
-  //     );
-  //   } else {
-  //     Notify.success(`Hooray! We found ${data.totalHits} images.`);
-  //   }
-
-  //   refs.loadMoreBtn.classList.remove('is-hidden');
-  //   return data.hits.reduce((markup, hit) => createMarkup(hit) + markup, '');
-  // })
-
-  // .then(updatePicturesList)
-  // .catch(onEror);
 }
 
 function createMarkup({
@@ -82,7 +72,7 @@ function createMarkup({
   comments,
   downloads,
 }) {
-  return `
+  return `<div class=" photo-card">
    <a class='gallery__link' href='${largeImageURL}'><img src="${webformatURL}" alt="${tags}" width=280px 
     height=200px;
  loading="lazy" /></a>
@@ -100,37 +90,38 @@ function createMarkup({
       <b>Downloads: ${downloads}</b>
     </p>
    </div>
- `;
+ </div>`;
+  }
+
+function updatePicturesList(result) {
+  refs.imageWrapper.insertAdjacentHTML('beforeend', result);
+  lightbox.refresh();
   
 }
 
-function updatePicturesList(markup) {
-
-  refs.imageWrapper.insertAdjacentHTML('beforeend', markup);
-  
-   console.log(markup);
-}
-
-function onLoadMore() {
+async function onLoadMore() {
   page += 1;
   lightbox.refresh();
-  getPicturesService
-    .getPictures(searchValue, page)
-    .then(data => {
-      // console.log(per_page)
-      if (data.hits.length === 0) {
-        refs.loadMoreBtn.classList.add('is-hidden');
-        Notify.info(
-          'Sorry, there are no images matching your search query. Please try again.'
-        );
-      } 
-      return data.hits.reduce((markup, hit) => createMarkup(hit) + markup, '');
-    })
-
-    .then(updatePicturesList)
-
-    .catch(onEror);
+  const pictures = await getPicturesService.getPictures(searchValue, page)
+  console.log(pictures);
+  try {
+    if (pictures.hits.length === 0) {
+      refs.loadMoreBtn.classList.add('is-hidden');
+      Notify.info(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+    }
+    const result=pictures.hits.reduce((markup, hit) => createMarkup(hit) + markup, '');
+    updatePicturesList(result)
+    
+  }
+  catch {
+    Notify.failure(
+      "We're sorry, but you've reached the end of search results."
+    );
+  }
 }
+
 
 function clearPicturesList() {
   refs.imageWrapper.innerHTML = '';
